bf85b6cbf367f6a720c1e97ed507c794245d43f2
Hamid <alhamid.mustholih97@gmail.com>
2024-11-18 16:04:43 +0700

    Menambahkan sections untuk halaman Home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import Navbar from '../Components/Navbar'
+import SectionHero from '../Sections/Home/SectionHero'
+import Footer from '../Components/Footer'
+import SectionTentang from '../Sections/Home/SectionTentang'
+import SectionLayanan from '../Sections/Home/SectionLayanan'
+import SectionKeunggulan from '../Sections/Home/SectionKeunggulan'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+        <Navbar/>
+        <SectionHero/>
+        <SectionTentang/>
+        <SectionLayanan/>
+        <SectionKeunggulan/>
+        <Footer/>
+    </>
   )
 }
