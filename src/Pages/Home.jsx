86e7384bbb98f86be4fcf01128719d5daf7075dd
Hamid <alhamid.mustholih97@gmail.com>
2024-11-19 21:27:58 +0700

    Menambahkan components Navbar2 dan Footer2

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,16 +5,18 @@ import Footer from '../Components/Footer'
 import SectionTentang from '../Sections/Home/SectionTentang'
 import SectionLayanan from '../Sections/Home/SectionLayanan'
 import SectionKeunggulan from '../Sections/Home/SectionKeunggulan'
+import Navbar2 from '../Components/Navbar2'
+import Footer2 from '../Components/Footer2'
 
 export default function Home() {
   return (
     <>
-        <Navbar/>
+        <Navbar2/>
         <SectionHero/>
         <SectionTentang/>
         <SectionLayanan/>
         <SectionKeunggulan/>
-        <Footer/>
+        <Footer2/>
     </>
   )
 }
