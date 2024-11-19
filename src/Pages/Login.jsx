116823d61a66eb27786db2e53b63bb5077e6c038
Hamid <alhamid.mustholih97@gmail.com>
2024-11-19 19:31:57 +0700

    Menambahkan sections untuk halaman Login dan Daftar

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Navbar from '../Components/Navbar'
+import SectionLogin from '../Sections/Login/SectionLogin'
+import Footer from '../Components/Footer'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <>
+      <Navbar/>
+      <SectionLogin/>
+      <Footer/>
+    </>
   )
 }
